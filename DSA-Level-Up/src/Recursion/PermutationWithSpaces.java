9e0eca6dc93e91d428a87c10e0c3f704f81515c5
uzumaki0207 <tripathihrishabh15@gmail.com>
2022-07-28 13:41:37 +0530

    After a pause

---
+++ DSA-Level-Up/src/Recursion/PermutationWithSpaces.java
@@ -0,0 +1,33 @@
+/*
+Enter Spaces between string characters.
+I/P :- A string :- ABC
+O/P :- A_B_C , AB_C , A_BC , ABC
+ */
+
+package Recursion;
+
+public class PermutationWithSpaces {
+    public static void main(String[] args) {
+        String input = "ABC";
+        String output = String.valueOf(input.charAt(0));
+        input = input.substring(1);
+        solve(input,output);
+
+    }
+
+    private static void solve(String input, String output) {
+        if(input.length()==0){
+            System.out.println(output);
+            return;
+        }
+        String output1 = output;
+        output1 += input.charAt(0);
+        String output2 = output;
+        output2 += "_";
+        output2 += input.charAt(0);
+        input = input.substring(1);
+        solve(input,output1);
+        solve(input,output2);
+
+    }
+}
---
+++ DSA-Level-Up/src/Recursion/UniqueSubsets.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+public class UniqueSubsets {
+    public static void main(String[] args) {
+        String input = "aac";
+        String output = "";
+        solve(input,output);
+    }
+
+    private static void solve(String input, String output) {
+        if(input.length()==0){
+            System.out.println(output);
+            return;
+        }
+        String output2 = output;
+        output2+=input.charAt(0);
+        input = input.substring(1);
+        solve(input, output);
+        solve(input,output2);
+    }
+}
